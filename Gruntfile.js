1d0287d37a70666c6821fbf4fef9fc17cf13b84d
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-08-02 17:47:17 +0200

    fix: remove jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,19 +87,6 @@ module.exports = function (grunt) {
             }
         },
 
-        // JS Check
-        jshint: {
-            options: {
-                jshintrc: ".jshintrc",
-                reporter: require("jshint-stylish")
-            },
-            js: [
-                "<%= srcdir %>/*.js",
-                "<%= srcdir %>/*/*.js",
-                "!<%= srcdir %>/**/*.spec.js"
-            ]
-        },
-
         // Check complexity
         complexity: {
             generic: {
@@ -180,7 +167,6 @@ module.exports = function (grunt) {
     grunt.registerTask("test", function () {
         grunt.task.run([
             "clean",
-            "jshint",
             "eslint",
             "complexity"
 
